5c6360f6d63b71c3d4157ba1302c017f408b3648
OlegBurkhan <90616891+OlegBurkhan@users.noreply.github.com>
2021-10-16 19:06:33 +0300

    array.php points 1-11 done

--- VariableLesson/array.php
+++ VariableLesson/array.php
@@ -16,14 +16,20 @@ echo "<ul><li> $SortByAlphabet[0], <li>$SortByAlphabet[1], <li>$SortByAlphabet[2
 //print_r($SortByAlphabet);
 
 // 3. Get new array but without the first and last items
+//$ArrWithoutFirstAndLast = $SortByAlphabet;
+//$ArrWithoutFirst = array_shift($ArrWithoutFirstAndLast);
+//$Reversed = array_reverse($ArrWithoutFirstAndLast);
+//$ArrWithoutLast = array_shift($Reversed);
+//$ArrWithoutFirstAndLast = $Reversed;
+//sort($ArrWithoutFirstAndLast);
+//echo "<ul><li> $ArrWithoutFirstAndLast[0], <li>$ArrWithoutFirstAndLast[1], <li>$ArrWithoutFirstAndLast[2], <li>$ArrWithoutFirstAndLast[3], </li></ul>";
+//Too much code, next one lookslike better
+
 $ArrWithoutFirstAndLast = $SortByAlphabet;
 $ArrWithoutFirst = array_shift($ArrWithoutFirstAndLast);
-$Reversed = array_reverse($ArrWithoutFirstAndLast);
-$ArrWithoutLast = array_shift($Reversed);
-$ArrWithoutFirstAndLast = $Reversed;
+$ArrWithoutLast = array_pop($ArrWithoutFirstAndLast);
 sort($ArrWithoutFirstAndLast);
 echo "<ul><li> $ArrWithoutFirstAndLast[0], <li>$ArrWithoutFirstAndLast[1], <li>$ArrWithoutFirstAndLast[2], <li>$ArrWithoutFirstAndLast[3], </li></ul>";
-//print_r($ArrWithoutFirstAndLast);
 
 
 // 4. Get new array but use only even items from the names array
@@ -67,36 +73,30 @@ echo "<br>";
 $products = [['name' => 'Samsung TV', 'price' => 2000.0000, 'stock' => 200],
     ['name' => 'Sony TV', 'price' => 2500.00, 'stock' => 10],
     ['name' => 'LG', 'price' => 950.0000, 'stock' => 1500]];
-echo "<table >
+
+
+function Print_Table_From_Array ($a){
+
+    echo '<table >
   <style>
 table, th, td {
   border:1px solid black;
-  //width:100%;  
+  //width:100%;
 }
-</style>
+</style>';
+    echo "<tr>";
 
-  <tr>
-    <th>Name</th>
-    <th>Price</th>
-    <th>Stock</th>
-  </tr>
-  <tr>
-    <td>{$products['0']['name']}</td>
-    <td>{$products['0']['price']}</td>
-    <td>{$products['0']['stock']}</td>
-  </tr>
-  <tr>
-    <td>{$products['1']['name']}</td>
-    <td>{$products['1']['price']}</td>
-    <td>{$products['1']['stock']}</td>
-  </tr>
-  <tr>
-    <td>{$products['2']['name']}</td>
-    <td>{$products['2']['price']}</td>
-    <td>{$products['2']['stock']}</td>
-  </tr>
-</table>";
-echo "<br>";
+    echo "<th>Name</th> <th>Price</th> <th>Stock</th>";
+    echo "</tr>";
+    foreach ($a as $key => $value) {
+        foreach ($value as $data)
+            echo "<td>" . $data . "</td>";
+        echo "</tr>";
+    }
+    echo "</table>";
+    echo "<br>";
+}
+Print_Table_From_Array ($products);
 
 // 7. Sort the array by the price (new array should start from lower prices) and show the result via the function below
 
@@ -106,47 +106,7 @@ function sort_by_price($a, $b) {
 
 $productsprice = $products;
 usort($productsprice, 'sort_by_price');
-
-echo "<table >
-  <style>
-table, th, td {
-  border:1px solid black;
-  //width:100%;  
-}
-</style>
-
-  <tr>
-    <th>Name</th>
-    <th>Price</th>
-    <th>Stock</th>
-  </tr>
-  <tr>
-    <td>{$productsprice['0']['name']}</td>
-    <td>{$productsprice['0']['price']}</td>
-    <td>{$productsprice['0']['stock']}</td>
-  </tr>
-  <tr>
-    <td>{$productsprice['1']['name']}</td>
-    <td>{$productsprice['1']['price']}</td>
-    <td>{$productsprice['1']['stock']}</td>
-  </tr>
-  <tr>
-    <td>{$productsprice['2']['name']}</td>
-    <td>{$productsprice['2']['price']}</td>
-    <td>{$productsprice['2']['stock']}</td>
-  </tr>
-</table>";
-echo "<br>";
-
-// 8. Create a function which calculate a sum of the all prices in the array
-
-function sum_of_price ($a){
-    $b = array_sum(array_column($a,'price'));
-    return ($b);
-}
-$SumOfPrice = sum_of_price($products);
-echo $SumOfPrice;
-echo "<br>";
+Print_Table_From_Array ($productsprice);
 
 // 9. Add the new items in the array ['name' => 'Panasonic', 'price' => 4000.0000, 'stock' => 0], ['name' => 'JVC', 'price' => 550.0000, 'stock' => 12]
 $newproducts = $products;
@@ -172,6 +132,20 @@ echo "<br>";
 // 11. Create a function taht returns an array with 20 items there the value is a random letter from latin alphabet (the value should not be repeated) 
 // Example ['s', 't', 'z' ...];
 
+function create_random_arr($a){
+    $RandomArrayAtoZ = range('a', 'z');
+    shuffle($RandomArrayAtoZ);
+    $c = array_slice($RandomArrayAtoZ,0, 20);
+    return $c;
+}
+$EmptyArray = [];
+$CreatedArray = create_random_arr($EmptyArray);
+echo "<pre>";
+print_r($CreatedArray);
+echo "</pre>";
+echo "<br>";
+
+
 // 12. Challenge task
 /*
 You have a store which sells beer but you don't sell a single bottle you can sell only packs
@@ -180,7 +154,8 @@ const SMALL_PACK = 3;
 const MIDDLE_PACK = 6;
 const BIG_PACK = 20;
 
-You need to create a function that get a int bottles as an argument and returns an optimized count of packs which need to buy for this
+You need to create a function that get a int bottles as an argument and returns
+an optimized count of packs which need to buy for this
 
 function calcPacks(int $bottles): array
 {
